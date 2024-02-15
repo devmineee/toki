ebb5b6724252eafb38cc601719ef14634e5cbf9f
mudokja <mudokja@gmail.com>
2024-02-16 02:51:15 +0900

    test: 이상한점 발견
    Signed-off-by: mudokja <mudokja@gmail.com>

--- backend/src/main/java/com/toki/backend/room/repository/RoomRepository.java
+++ backend/src/main/java/com/toki/backend/room/repository/RoomRepository.java
@@ -1,19 +1,15 @@
 package com.toki.backend.room.repository;
 
-import com.toki.backend.room.dto.RoomInfoDto;
 import com.toki.backend.room.entity.Category;
 import com.toki.backend.room.entity.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, String> {
-    Optional<Room> findByParentRoomPk(String parentRoomPk);
+    Optional<Room> findByParentRoomPk(Room parentRoomPk);
     Page<Room> findAllByCategory(PageRequest pageRequest, Category category);
 
 }
--- backend/src/main/java/com/toki/backend/room/service/RoomService.java
+++ backend/src/main/java/com/toki/backend/room/service/RoomService.java
@@ -135,7 +135,7 @@ public class RoomService {
     public CreateRoomResponseDto saveRoom(CreateRoomRequestDto createRoomRequestDto, String hostUserPk) {
 
         Room room = Room.builder()
-                .parentRoomPk(roomRepository.findByParentRoomPk(createRoomRequestDto.getParentRoomId()).orElse(null))
+                .parentRoomPk(roomRepository.findByParentRoomPk(Room.builder().roomPk(createRoomRequestDto.getParentRoomId()).build()).orElse(null))
                 .title(createRoomRequestDto.getRoomName())
                 .category(categoryRepository.findById(createRoomRequestDto.getCategoryPk()).get())
                 .isPrivate(createRoomRequestDto.getIsPrivate())
